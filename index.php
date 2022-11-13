d52c91e735075edd77e300413cb18b69f37afa0e
root <root@DESKTOP-SIKK61N.localdomain>
2022-11-13 19:35:16 +0800

    1st push

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$temp=uniqid().".txt";
+$request=file_get_contents("php://input");
+file_put_contents($temp, $request);
+exec("/mnt/c/Windows/system32/clip.exe < $temp");
+unlink($temp);
+echo strlen($request)."\n";
+?>

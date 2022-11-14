1567354e33d4739914cf7c8c6fd9f858579d9654
pank <pank@pank.org>
2022-11-14 10:24:11 +0800

    20221114-1024

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $request=file_get_contents("php://input");
 file_put_contents($temp, $request);
 exec("/mnt/c/Windows/system32/clip.exe < $temp");
 unlink($temp);
-echo strlen($request)."\n";
+echo strlen($request)." bytes sent\n";
 ?>
